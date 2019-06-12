8d19a67b0a99db6ecb110a6fd4c61c2c0b0e1a36
Paul Sherman <paul.sherman.88@gmail.com>
2019-06-12 13:06:22 -0500

    Verify BINARY chaining works

--- tests/types/expressions.spec.ts
+++ tests/types/expressions.spec.ts
@@ -22,4 +22,17 @@ describe("BINARY", () => {
       stringify`${value}`
     ).toBe("value < 7;");
   });
+
+  it("correctly chains binary expressions", () => {
+    const value = types.AS_STATEMENT(
+      types.BINARY(
+        types.BINARY(types.NUMBER(6), "+", types.NUMBER(7)),
+        "*",
+        types.NUMBER(3)
+      )
+    );
+    expect(
+      stringify`${value}`
+    ).toBe("(6 + 7) * 3;");
+  });
 });
